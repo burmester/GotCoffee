9452cbcf79a4985f507d77dc7886b9dae078cb41
Johan Burmester <johan.burmester@unikum.net>
2015-05-17 18:02:10 +0200

    require setuped

---
+++ client/config.js
@@ -0,0 +1,6 @@
+require.config({
+    paths: {
+        'jquery': 'scripts/vendor/jquery',
+        'less': 'scripts/vendor/less'
+    }
+});
--- client/scripts/main.js
+++ client/scripts/main.js
@@ -1,14 +1,17 @@
-window.onload = function () {
-    var url, i;
+define(function (require) {
+    var $ = jQuery = require('jquery'),
+        less = require('less');
 
-    for (i = 0; i < 2; i++) {
-        url = document.URL + 'inputs/' + i;
-        $.getJSON(url, function (data) {
-            console.log('API response received');
-            $('#input').append('<p>input gpio port ' + data.gpio + ' on pin ' + data.pin + ' has current value ' + data.value + '</p>');
-        });
-    }
-};
+    $(document).ready(function () {
+        var url, i;
 
+        for (i = 0; i < 2; i++) {
+            url = document.URL + 'inputs/' + i;
+            $.getJSON(url, function (data) {
+                console.log('API response received');
+                $('#input').append('<p>input gpio port ' + data.gpio + ' on pin ' + data.pin + ' has current value ' + data.value + '</p>');
+            });
+        }
+    });
 
-
+});
